03b96d3c121bea09d026356bbd3557b5be231601
Jaap <jaap@mrproper.dev>
2022-02-12 11:10:44 +0100

    Fix ipfs solver

--- helpers/ipfsSolver.js
+++ helpers/ipfsSolver.js
@@ -1,13 +1,13 @@
 function parseIPFSUrl(url) {
-  if (!url) return url;
+  if (!url || url.startsWith('https://') || url.startsWith('http://')) return url;
 
   if (url.startsWith('ipfs://')) {
     url = url.split('ipfs://')[1];
   } else if (url.startsWith('https://ipfs.io/ipfs/')) {
     url = url.split('https://ipfs.io/ipfs/')[1];
   }
-  if (url.includes('ipfs/')) {
-    url = url.split('ipfs/')[1];
+  if (url.startsWith('/ipfs/')) {
+    url = url.split('/ipfs/')[1];
   }
   url = `https://cloudflare-ipfs.com/ipfs/${url}`;
   return url;
