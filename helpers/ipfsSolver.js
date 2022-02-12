107b9894c1ab8e1a2b665d3e9302d235fcef9d1a
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2022-02-12 13:24:33 +0100

    Add profile banner

--- helpers/ipfsSolver.js
+++ helpers/ipfsSolver.js
@@ -9,6 +9,9 @@ function parseIPFSUrl(url) {
   if (url.startsWith('/ipfs/')) {
     url = url.split('/ipfs/')[1];
   }
+  if (url.startsWith('ipfs/')) {
+    url = url.split('ipfs/')[1];
+  }
   if (!url.startsWith('https://') && !url.startsWith('http://')) {
     url = `https://gateway.ipfs.io/ipfs/${url}`;
   }
--- routes/account.js
+++ routes/account.js
@@ -30,9 +30,20 @@ router.get('/:chain/:address', async function (req, res, next) {
   }
 });
 
+async function getProfileBanner(address) {
+  console.log('Getting profile banner');
+  try {
+    const profileData = await axios.post(`https://api-mainnet.rarible.com/marketplace/api/v4/profiles/list`, [address]);
+    return IpfsSolver(profileData.data[0].cover);
+  } catch (e) {
+    return null;
+  }
+}
+
 async function fetchAccountInfo(address, chain) {
   const options = { chain: chain, address: address };
   const secondsSinceEpoch = Math.round(Date.now() / 1000);
+  const profileBanner = await getProfileBanner(address);
 
   try {
     let nftData = await Moralis.Web3API.token.getAllTokenIds(options);
@@ -42,7 +53,13 @@ async function fetchAccountInfo(address, chain) {
     }
     nftData = await enrichNFTData(nftData.result, chain);
 
-    let responseData = { nfts: nftData, chain: chain, address: address.toLowerCase(), cachedAt: secondsSinceEpoch };
+    let responseData = {
+      nfts: nftData,
+      chain: chain,
+      address: address.toLowerCase(),
+      cachedAt: secondsSinceEpoch,
+      profileBanner: profileBanner,
+    };
 
     await NFTProfileModel.updateOne({ chain: chain, address: address }, responseData, { upsert: true, setDefaultsOnInsert: true });
 
